225b8f188062a20fdcb83ac451c0dddd1950cd5e
Nguyen Van Phuong <nguyenvanphuonga9@gmail.com>
2021-10-11 17:33:19 +0700

    query graphql

---
+++ src/graphql-client/queries.js
@@ -0,0 +1,31 @@
+import { gql } from '@apollo/client'
+
+const getBooks = gql`
+	query getBooksQuery {
+		books {
+			name
+			id
+		}
+	}
+`
+
+const getSingleBook = gql`
+	query getSingleBookQuery($id: ID!) {
+		book(id: $id) {
+			id
+			name
+			genre
+			author {
+				id
+				name
+				age
+				books {
+					id
+					name
+				}
+			}
+		}
+	}
+`
+
+export { getBooks, getSingleBook };
